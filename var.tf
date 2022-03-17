761e6086ddaf75e6bbbe302c674e1191818d1bcb
Sunit Jain <mail4sunit@gmail.com>
2022-03-17 13:58:50 -0700

    customized

--- var.tf
+++ var.tf
@@ -1,19 +1,28 @@
+variable "region" {
+  default = "us-west-2"
+  type    = string
+}
 variable "availability_zone" {
   default = "us-west-2a"
+  type    = string
 }
 
 variable "contact" {
-  default = "blake.cannon@caprinomics.com"
+  default = "contact@sunitjain.com"
+  type    = string
 }
 
 variable "environment" {
-  default = "TEST"
+  default = "Devlopment"
+  type    = string
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  default = "r4.2xlarge"
+  type    = string
 }
 
 variable "service" {
   default = "jupyter"
+  type    = string
 }
