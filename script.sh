2f88ebb713e522020aa1cea9cf3f71114f048bb9
Sunit Jain <mail4sunit@gmail.com>
2022-03-25 11:48:28 -0700

    install efs utils and mount efs if exists

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sleep 1m
+USEEFS="fs-affa1da9"
 # Log stdout to file
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
@@ -10,11 +11,12 @@ sudo yum update -y
 aws --version
 sudo yum remove -y awscli || echo "aws not installed using yum"
 # Install/update awscli2
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-sudo ln -s  /usr/local/aws-cli/v2/current/bin/aws /usr/bin/aws
-aws --version
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+unzip -q /tmp/awscliv2.zip -d /tmp
+/tmp/aws/install -b /usr/bin
+# check that the aws-cli was actually installed. if not shutdown (terminate) the instance
+command -v aws || shutdown -P now
+sudo yum install -y amazon-efs-utils
 # Mount /anaconda3
 sudo mkfs.xfs /dev/sdb -f
 sudo mkdir /anaconda3
@@ -33,3 +35,10 @@ runuser -l ec2-user -c 'jupyter notebook --generate-config' &&
     sed -i -e "s/# c.NotebookApp.ip = 'localhost'/c.NotebookApp.ip = '"$(curl http://169.254.169.254/latest/meta-data/public-hostname)"'/g" /home/ec2-user/.jupyter/jupyter_notebook_config.py &&
     sed -i -e "s/# c.NotebookApp.allow_origin = ''/c.NotebookApp.allow_origin = '*'/g" /home/ec2-user/.jupyter/jupyter_notebook_config.py &&
     sed -i -e "s/# c.NotebookApp.open_browser = True/c.NotebookApp.open_browser = False/g" /home/ec2-user/.jupyter/jupyter_notebook_config.py
+# [OPTIONAL] Mount existing EFS
+if [[ $USEEFS != "none" ]]
+then
+    mkdir -p /mnt/efs
+    echo $USEEFS:/ /mnt/efs efs tls,_netdev >> /etc/fstab
+    mount -a -t efs defaults
+fi
